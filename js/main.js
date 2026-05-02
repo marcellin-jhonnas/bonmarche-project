904ab366168ee88da4fd744d8871015615abdcef
marcellin-jhonnas <marcellinjhonnas@gmail.com>
2026-05-02 18:00:22 +0000

    Ajout Performance et Caisse

--- js/main.js
+++ js/main.js
@@ -584,7 +584,7 @@ function afficherPanier() {
                 <span>Articles :</span> <span>${sousTotal.toLocaleString()} Ar</span>
             </div>
             <div style="display:flex; justify-content:space-between; color:#d35400; font-weight:bold; margin-top:5px;">
-                <span>Livraison (15%) :</span> <span>+ ${fraisLivraison.toLocaleString()} Ar</span>
+                <span>Frais de Livraison :</span> <span>+ ${fraisLivraison.toLocaleString()} Ar</span>
             </div>
         </div>` : ''}
     `;
